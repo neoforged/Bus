cb38ccd41626ea2bb5f8c350a3d57b20c59a6fe3
Matyrobbrt <matyrobbrt@gmail.com>
2023-08-14 22:07:10 +0300

    7.0 and Neoify

--- bus-testjars/src/main/java/module-info.java
+++ bus-testjars/src/main/java/module-info.java
@@ -1,5 +1,5 @@
-open module net.minecraftforge.eventbus.testjars {
-    requires net.minecraftforge.eventbus;
+open module net.neoforged.bus.testjars {
+    requires net.neoforged.bus;
 
     exports net.minecraftforge.eventbus.testjar;
     exports net.minecraftforge.eventbus.benchmarks.compiled;
