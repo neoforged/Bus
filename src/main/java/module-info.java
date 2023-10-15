2009fcf9c87591cecbdad6ed661ad5273d8d0864
Technici4n <13494793+Technici4n@users.noreply.github.com>
2023-10-15 23:07:21 +0200

    Fix tests by adding `jdk.unsupported` dependency

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ open module net.neoforged.bus {
     requires org.apache.logging.log4j;
     requires static org.jetbrains.annotations;
     requires net.jodah.typetools;
+    requires jdk.unsupported; // required for typetools
 
     exports net.neoforged.bus;
     exports net.neoforged.bus.api;
