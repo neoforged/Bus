98a62a3d95662236e71bc8bedd8d92f2ccdcb1f4
Matyrobbrt <matyrobbrt@gmail.com>
2023-10-30 11:29:35 +0200

    Get rid of the jopt dep and pin typetools

--- bus-test/src/test/java/module-info.java
+++ bus-test/src/test/java/module-info.java
@@ -3,7 +3,6 @@ open module net.neoforged.bus.test {
     requires org.apache.logging.log4j;
     requires org.apache.logging.log4j.core;
     requires net.neoforged.bus;
-    requires jopt.simple;
 
     requires static org.jetbrains.annotations;
 
